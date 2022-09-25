31765b38a623ef460fee2fbaa37511231d5bcb12
clinfc <1962109568@qq.com>
2022-09-25 16:40:55 +0800

    no message

---
+++ element-plus/assets/IPrism.ceb4305d.js
@@ -0,0 +1,1 @@
+import{_ as a,ao as r,o as n,c,a as l,v as e,P as t,b as i}from"./index.8da1a921.js";const _=["innerHTML"];function p(s,m,d,f,u,$){const o=i("i-scroll");return n(),c(o,{class:"exc-prism","scroll-x":"","scroll-y":""},{default:l(()=>[e("pre",{class:t(`language-${s.lang}`)},[e("code",{innerHTML:s.html},null,8,_)],2)]),_:1})}const x=a(r,[["render",p]]);export{x as I};
